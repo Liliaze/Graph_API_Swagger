d58e4b08d44eed1b3a036c6e893f68b148fd1752
BOUDY Diana <diana.boudy@gmail.com>
2017-01-18 10:42:35 +0000

    new calcul to getSharePriceList with more realistics results

--- api/controllers/companySharePrice.js
+++ api/controllers/companySharePrice.js
@@ -7,11 +7,20 @@ module.exports = {
 function getCompanySharePrice(req, res) {
   var list = []
   var date = new Date(req.swagger.params.date.value)
-
+  var amountTest = Math.random() * 100
+  var test = 0
+  
   for (var i = 0; i < 24; i++) {
+    test = Math.random() * 100
+    console.log(test&1)
+    if ((test & 1) == 0)
+      amountTest += amountTest * 15 / 100;
+    else
+      amountTest -= amountTest * 15 / 100;
+     
     list[list.length] = {
       time: new Date( date - (24 * 3600 * 1000) + i * (3600 * 1000) ),
-      amount: Math.random() * 100
+      amount: amountTest
     }
   }
 
